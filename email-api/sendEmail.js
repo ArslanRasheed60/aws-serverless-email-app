4ffe298d930070547fe66f0edb240112c5e738b9
ArslanRasheed60 <arslanrashee1@gmail.com>
2023-07-15 23:00:42 +0500

    sendEmail added

---
+++ email-api/sendEmail.js
@@ -0,0 +1,64 @@
+const AWS = require("aws-sdk");
+
+const ses = new AWS.SES();
+
+//* create lamda function
+exports.handler = async (event) => {
+  const { to, from, subject, text } = JSON.parse(event.body);
+
+  if (!to || !from || !subject || !text) {
+    return _400({
+      message: "missing parameters on request body",
+    });
+  }
+
+  const emailParams = {
+    Destinaion: {
+      ToAddresses: [to],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: text,
+        },
+      },
+      Subject: {
+        Data: subject,
+      },
+    },
+    Source: from,
+  };
+
+  try {
+    await ses.sendEmail(emailParams).promise();
+    return _200();
+  } catch (error) {
+    console.log("error sending email", error);
+
+    return _400({ message: "unable to send an email" });
+  }
+};
+
+const _400 = (body) => {
+  return {
+    Headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Origin": "*",
+    },
+    statusCode: 400,
+    body: JSON.stringify(body),
+  };
+};
+
+const _200 = (body) => {
+  return {
+    Headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Origin": "*",
+    },
+    statusCode: 200,
+    body: "",
+  };
+};
